90334b8b6974c6dfec114d2978a381c4bf115f68
joschi127 <joschi127@users.noreply.github.com>
2018-07-28 15:33:34 +0200

    Update build.sh script
    Remove old local images, if they are not used, add nicer msg

--- build.sh
+++ build.sh
@@ -33,5 +33,5 @@ docker push joschi127/php-plus-nvm:7.2.1-apache_latest
 # remove old local images, if they are not used
 for old_image_id in $(docker images | grep joschi127/php-plus-nvm | grep -v _latest | grep -v _$buildnumber | awk '{print $3}')
 do
-    docker rmi $old_image_id || true
+    docker rmi $old_image_id || echo "Keeping image $old_image_id, seems to be still in use"
 done
